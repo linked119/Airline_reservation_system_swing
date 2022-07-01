81c70863af5193b0de69f1ad5d64c3a903f3adf5
linked119 <anasansari03412350566@gmail.com>
2022-07-01 13:41:16 +0500

    completed

---
+++ src/test.java
@@ -0,0 +1,13 @@
+// Java Program to Retrieve Contents of a Table Using JDBC
+// connection
+
+// Showing linking of created database
+// Importing SQL libraries to create database
+import java.sql.*;
+
+public class test {
+
+    public static void main(String[] args) {
+        
+    }
+}
